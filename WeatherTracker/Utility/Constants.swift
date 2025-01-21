b680c623bdfca2f417b447f5c9574bee5e482c1a
Amjad Khan <khan.amjad36@gmail.com>
2025-01-21 07:47:05 +0530

    persist searched city

--- WeatherTracker/Utility/Constants.swift
+++ WeatherTracker/Utility/Constants.swift
@@ -8,4 +8,5 @@
 public enum Constants {
     static let apiKey = "f252baafbd7e425697f114913251701"
     static let baseURL = "http://api.weatherapi.com/v1"
+    static let cityNameKey = "CityName"
 }
